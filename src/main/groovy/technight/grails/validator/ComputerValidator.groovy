08e9a30df51f29154b6ead3f81d80077eef8652e
Valjery W Dorceus <dwelinghton@excilys.com>
2019-07-25 11:28:39 +0200

    added Computer validation class

---
+++ src/main/groovy/technight/grails/validator/ComputerValidator.groovy
@@ -0,0 +1,16 @@
+package technight.grails.validator
+
+class ComputerValidator {
+
+    static introduced = { val, obj, error ->
+
+        return true
+
+    }
+
+    static discontinued = { val, obj, error ->
+
+        return true
+
+    }
+}
